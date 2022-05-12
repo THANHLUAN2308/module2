daeaf2516a447a950947a88c75d34d8114bb37a4
THANHLUAN2308 <thaicongthanhluan@gmail.com>
2022-05-12 14:53:10 +0700

    dualen

---
+++ bai2/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    String Fullname;
+    short age;
+    boolean isDead;
+
+    public static void main(String[] args) {
+        Employee myEmployee=new Employee();
+        System.out.println(myEmployee.Fullname);
+        System.out.println(myEmployee.age);
+        System.out.println(myEmployee.isDead);
+
+    }
+}
