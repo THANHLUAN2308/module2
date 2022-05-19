67c02566c8575e953ae169b5b00f6f14318e27cd
THANHLUAN2308 <thaicongthanhluan@gmail.com>
2022-05-19 09:09:22 +0700

    dualen

---
+++ day-of-month/src/com/codegym/dayofmonth.java
@@ -0,0 +1,32 @@
+package com.codegym;
+import java.util.Scanner;
+public class dayofmonth {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Which month that you want to count days?");
+        int month = scanner.nextInt();
+        switch (month) {
+            case 2:
+                System.out.printf("The month '2' has 28 or 29 days");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.printf("The month '" + month + "' has 31 days!");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.printf("The month '" + month + "' has 30 days!");
+                break;
+            default:
+                System.out.printf("Invalid input!");
+        }
+
+    }
+}
